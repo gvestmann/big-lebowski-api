ad5174f472bf8aa3f4acf6b690cc139e8b92a040
gvestmann <gvestmann@icloud.com>
2021-01-18 14:26:30 +0100

    trying to fix cors

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const importData = require("./data.json")
 let port = process.env.PORT || 3000;
+
+app.use(cors())
 
 app.get("/", (req, res) => {
     res.send(importData)
