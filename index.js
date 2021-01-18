5fb25b084f5cadd1fb96a0289381eb1756f0693c
gvestmann <gvestmann@icloud.com>
2021-01-18 13:44:59 +0100

    removed cors thingy messing up my server

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const importData = require("./data.json")
 let port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    response.set('Access-Control-Allow-Origin', '*');
     res.send(importData)
 });
 
