be3425551e4131d4ea74fe84acffe372ef469d17
gvestmann <gvestmann@icloud.com>
2021-01-18 13:40:37 +0100

    fourth-commit

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const importData = require("./data.json")
 let port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
+    response.set('Access-Control-Allow-Origin', '*');
     res.send(importData)
 });
 
