2fa2a4f08ffb869fd9ee78bcf54e67019c87a98f
gvestmann <gvestmann@icloud.com>
2021-01-18 12:12:28 +0100

    second commit

--- index.js
+++ index.js
@@ -4,12 +4,9 @@ const importData = require("./data.json")
 let port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hi there!")
-});
-
-app.get("/people", (req, res) => {
     res.send(importData)
 });
+
 
 app.listen(port, () => {
     console.log(`Example App is listening on http:localhost:${port}`);
