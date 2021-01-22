85ab6e64bb133797adb8e2350e2df1527161a041
gvestmann <gvestmann@icloud.com>
2021-01-22 12:56:41 +0100

    Added comments to code and updated README

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # big-lebowski-api
+
+## An API dedicated to The Big Lebowski's characters
+
+This simple API is based on Node + Express and is deployed to Heroku. It also uses NPM-module "Cors" for solving some cross-origin problems I was having. It feeds JSON-data to the live APIs' main route.
+
+###### Links of interests
+* [Live API](https://thelewbowskiapi.herokuapp.com/)
+* [Front-end website](https://gvestmann.github.io/the-big-laboutski/)
+* [Front-end's repo](https://github.com/gvestmann/the-big-laboutski)
+
+... fuck it, Dude. Let's go bowling.
--- index.js
+++ index.js
@@ -1,15 +1,27 @@
+// Here's a variable for importing Express to the app
 const express = require('express');
+
+// This one imports the cors-module
 const cors = require('cors');
+
+// The app then uses Express
 const app = express();
+
+// This variable references the API's data
 const importData = require("./data.json")
+
+// For Heroku to work, you need to tell it to listen to it's default enviroment's port || or ... 3000 for local development
 let port = process.env.PORT || 3000;
 
+// I spent almost two days on trying to fix this cors shit. I eventually gave up and contacted Smári. We fixed it in 10 minutes or so. Lesson learnt. Stupid, stupid Gummi
 app.use(cors())
 
+// This is the router, it imports the data to my API's main route
 app.get("/", (req, res) => {
     res.send(importData)
 });
 
+// This is just for simplicities sake in the local development enviroment to show me which port is being used and providing a link to it
 app.listen(port, () => {
-    console.log(`Example App is listening on http:localhost:${port}`);
+    console.log(`DEV app is listening on http:localhost:${port}`);
 });
